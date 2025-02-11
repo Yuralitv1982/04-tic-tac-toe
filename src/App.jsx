eb0c74c6c054bdd4a3e414316bfed3f4a4428eb8
Yuralitv1982 <yuralitv02@gmail.com>
2025-02-11 18:15:18 +0200

    ch 4 video 35 player name in window

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,9 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+   const [players, setPlayers] = useState({ X: 'Player 1', O: 'Player 2' });
    const [gameTurns, setGameTurns] = useState([]);
-   const [hasWinner, setWinner] = useState(false);
+   // const [hasWinner, setWinner] = useState(false);
    // const [activePlayer, setActivePlayer] = useState('X');
 
    const activePlayer = deriveActivePlayer(gameTurns);
@@ -51,7 +52,7 @@ function App() {
          firstSquareSymbol === secondSquareSymbol &&
          firstSquareSymbol === thirdSquareSymbol
       ) {
-         winner = firstSquareSymbol;
+         winner = players[firstSquareSymbol];
       }
    }
 
@@ -73,6 +74,15 @@ function App() {
       setGameTurns([]);
    }
 
+   function handlePlayerNameChange(symbol, newName) {
+      setPlayers((prevPlayers) => {
+         return {
+            ...prevPlayers,
+            [symbol]: newName,
+         };
+      });
+   }
+
    return (
       <main>
          <div id='game-container'>
@@ -81,6 +91,7 @@ function App() {
                   initialName='Player 1'
                   symbol='X'
                   isActive={activePlayer === 'X'}
+                  onChangeName={handlePlayerNameChange}
                />
                <Player
                   initialName='Player 2'
