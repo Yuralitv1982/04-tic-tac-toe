af689f0d31a435db1984df2a7ad939d62e9b200d
Yuralitv1982 <yuralitv02@gmail.com>
2025-02-11 17:38:55 +0200

    ch 4 video 32 implement draw

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Player from './component/Player';
 import GameBoard from './component/GameBoard';
 import Log from './component/Log';
+import GameOver from './component/GameOver';
 import { WINNING_COMBINATIONS } from './winning-combinations';
 
 const initialGameBoard = [
@@ -54,6 +55,8 @@ function App() {
       }
    }
 
+   const hasDraw = gameTurns.length === 9 && !winner;
+
    function handleSelectSquare(rowIndex, colIndex) {
       // setActivePlayer((curActivePlayer) =>curActivePlayer === 'X' ? 'O' : 'X');
       setGameTurns((prevTurns) => {
@@ -81,7 +84,7 @@ function App() {
                   isActive={activePlayer === 'O'}
                />
             </ol>
-            {winner && <p>You won,{winner}! </p>}
+            {(winner || hasDraw) && <GameOver winner={winner} />}
             <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
          </div>
          <Log turns={gameTurns} />
